54d9a76c25a40881ea6fba32b9319cff10afd4c5
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 10:24:53 -0600

    fix: clarified admission price variable

--- spec/national_park_spec.rb
+++ spec/national_park_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe NationalPark do
 
     it 'has a name and admission price' do
         expect(@rocky_mtn.name).to eq("Rocky Mountain National Park")
-        expect(@rocky_mtn.admission).to eq(20)
+        expect(@rocky_mtn.admission_price).to eq(20)
     end
 end
