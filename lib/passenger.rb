893fe9cc8299a690f6eecb1c5e59e84b62615bba
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 09:26:11 -0600

    feat: frame for pasenger class and methods needed

---
+++ lib/passenger.rb
@@ -0,0 +1,22 @@
+class Passenger
+
+    attr_reader :name,
+                :age
+
+    def initialize(passenger_data)
+        @name =
+        @age = 
+    end
+
+    def adult?
+
+    end
+
+    def driver?
+
+    end
+
+    def drive
+
+    end
+end
