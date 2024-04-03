c15cd8f02978b3aede915c3c5c6bc3393da67bab
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 10:04:33 -0600

    feat: passengers instance variable and methods needed for passengers

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,13 +3,15 @@ class Vehicle
     attr_reader :year,
                 :make,
                 :model,
-                :speeding
+                :speeding,
+                :passengers
 
     def initialize(year, make, model)
         @year = year
         @make = make
         @model = model
         @speeding = false
+        @passengers = []
     end
 
     def speeding?
@@ -18,5 +20,13 @@ class Vehicle
 
     def speed 
         @speeding = true
+    end
+
+    def add_passenger
+
+    end
+
+    def num_adults
+
     end
 end
