43972d10dba3101542c4ed554f6cad88a673859e
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 09:53:06 -0600

    test: speeding test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,7 +2,7 @@ require './lib/passenger'
 require './lib/vehicle'
 
 RSpec.describe Passenger do
-    
+
     before do
         @vehicle = Vehicle.new("2001", "Honda", "Civic")
     end
@@ -11,5 +11,11 @@ RSpec.describe Passenger do
         expect(@vehicle.year).to eq("2001")
         expect(@vehicle.make).to eq("Honda")
         expect(@vehicle.model).to eq("Civic")
+    end
+
+    it 'can check if vehicle is speeding and' do
+        expect(@vehicle.speeding?).to eq(false)
+        @vehicle.speed
+        expect(@vehicle.speeding?).to eq(true)
     end
 end
