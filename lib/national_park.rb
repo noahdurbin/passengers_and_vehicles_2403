54835f3b1e92914dcd5342938484262c41feb919
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 10:23:47 -0600

    feat: initialized with name and admission price

---
+++ lib/national_park.rb
@@ -0,0 +1,11 @@
+class NationalPark
+
+    attr_reader :name,
+                :admission
+
+
+    def initialize(name, admission)
+        @name = name
+        @admission = admission
+    end
+end
