50a9d09142a8b1270defa78c1769b1f18cb0df4a
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 09:19:05 -0600

    test: added a before block for tests and a test for name

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,5 +1,12 @@
 require './lib/passenger'
 
 RSPec.describe Passenger do
+    before do
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    end
 
+    it 'can recall the passengers name' do
+        expect(@charlie.name).to eq("Charlie")
+    end
 end
