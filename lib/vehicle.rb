b071ad4515b33d6df814ae503a24ee221aa02c0d
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 09:55:55 -0600

    feat: speeding instance variable and methods to checka nd alter that

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,11 +2,21 @@ class Vehicle
     
     attr_reader :year,
                 :make,
-                :model
+                :model,
+                :speeding
 
     def initialize(year, make, model)
         @year = year
         @make = make
         @model = model
+        @speeding = false
+    end
+
+    def speeding?
+        @speeding == true
+    end
+
+    def speed 
+        @speeding = true
     end
 end
