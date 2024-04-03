5fc81c90a62f4b30bbd0efc0e8396f90aee5e8ad
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 09:49:52 -0600

    fix:fixed typo and formatting mistakes

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,6 +1,7 @@
 require './lib/passenger'
 
 RSpec.describe Passenger do
+    
     before do
         @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
         @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,13 +2,14 @@ require './lib/passenger'
 require './lib/vehicle'
 
 RSpec.describe Passenger do
+    
     before do
         @vehicle = Vehicle.new("2001", "Honda", "Civic")
     end
 
     it 'vehicle initializes with year make and model' do
         expect(@vehicle.year).to eq("2001")
-        expect(@vehiclel.make).to eq("Honda")
+        expect(@vehicle.make).to eq("Honda")
         expect(@vehicle.model).to eq("Civic")
     end
 end
