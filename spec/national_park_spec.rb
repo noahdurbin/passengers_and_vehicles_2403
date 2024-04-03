7a8194a401ed12f05c2cc6838665d53652ae7b97
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 10:21:26 -0600

    test: test initialization and attr readers

--- spec/national_park_spec.rb
+++ spec/national_park_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe NationalPark do
         @rocky_mtn = NationalPark.new("Rocky Mountain National Park", 20)
         @glacier = NationalPark.new("Glacier National Park", 15)
     end
+
+    it 'has a name and admission price' do
+        expect(@rocky_mtn.name).to eq("Rocky Mountain National Park")
+        expect(@rocky_mtn.admission).to eq(20)
+    end
 end
