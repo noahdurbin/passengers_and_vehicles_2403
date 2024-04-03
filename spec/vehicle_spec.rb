893fc3d136793510aa259fcb9170810d3b8874c6
Noah Durbin <noahdurbin@gmail.com>
2024-04-03 09:59:58 -0600

    test: passengers and add passengers test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Passenger do
 
     before do
         @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        @jude = Passenger.new({"name" => "Jude", "age" => 20})
     end
 
     it 'vehicle initializes with year make and model' do
@@ -17,5 +20,13 @@ RSpec.describe Passenger do
         expect(@vehicle.speeding?).to eq(false)
         @vehicle.speed
         expect(@vehicle.speeding?).to eq(true)
+    end
+
+    it 'can hold passengers and initializes with no passengers' do
+        expect(@vehicle.passengers).to eq([])
+        @vehicle.add_passenger(@charlie)
+        @vehicle.add_passenger(@jude)
+        @vehicle.add_passenger(@taylor)
+        expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
     end
 end
